48a601521eff9202d7e8794db15c55e25d6dc9e6
Michael Campbell <michael.campbell14@gmail.com>
2020-03-11 12:49:16 -0400

    Updates

---
+++ precache-manifest.82477b07b0e068cfe984302a010ecf1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d02cd268dfd4579be5b943a8105b1a",
+    "url": "/learning-react1/index.html"
+  },
+  {
+    "revision": "d3de62c69a6234f4fd8d",
+    "url": "/learning-react1/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "badb2636bd56744507fa",
+    "url": "/learning-react1/static/js/2.417f3472.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/learning-react1/static/js/2.417f3472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3de62c69a6234f4fd8d",
+    "url": "/learning-react1/static/js/main.d91821e4.chunk.js"
+  },
+  {
+    "revision": "40d713bcfbb83c3bcb05",
+    "url": "/learning-react1/static/js/runtime-main.f29bc8f2.js"
+  }
+]);
